2d3d761f59027d9bc17db3adec320949b386158a
weixiao <weixiao@megvii.com>
2022-05-10 11:32:18 +0800

    fix: 修改promise实现

--- Promise A+ and static method/promise.js
+++ Promise A+ and static method/promise.js
@@ -1,108 +1,75 @@
-const PENDING = "pending";
-const FULFILLED = "fulfilled";
-const REJECTED = "rejected";
-
-const resolvePromise = (promise, x, resolve, reject) => {
-  // 1. promise===x reject错误
-  // 2. x是Promise函数, 将x的结果放入resolve和reject
-  // 3. x是普通值，将x直接resolve
-  if (promise === x) {
-    reject(TypeError("xxxxxx......"));
-    return;
-  }
-  // 一般会判断x是对象，且x.then是方法，主要是为了应对和其他满足promise规范发Promise混用时的情况，此处简单点判断。。
-  // if(x instanceof myPromise){
-  if (typeof x?.then === "function") {
-    x.then(
-      (res) => {
-        // 因为then函数return的是promise,因此x的状态需向上一层的promise传递
-        resolve(res);
-      },
-      (rej) => {
-        reject(rej);
-      }
-    );
-    return;
-  }
-  resolve(x);
+const statusEnum = {
+  PENDING: "pending",
+  FULLFILED: "fulfilled",
+  REJECTED: "rejected",
 };
-class myPromise {
-  static resolve(val) {
-    return new myPromise((res) => {
-      res(val);
-    });
+/**
+ *
+ * @param {any} p fn的返回值
+ * @param {Promise} p1 .then生成的新Promise
+ * @param {Function} resolve
+ * @param {Function} reject
+ */
+const resolvePromise = (promise, value, resolve, reject) => {
+  if (promise === value) {
+    throw TypeError("报错....");
+  } else if (typeof value?.then === "function") {
+    // 注意，这里值判断p.then，所有复合promise A+协议的promise实现都是可以互相调用的
+    value.then(resolve, reject);
+  } else {
+    resolve(value);
   }
+};
+class MyPromise {
+  status = statusEnum.PENDING;
+  resArr = [];
+  rejArr = [];
+  value = null;
+  reason = null;
   constructor(fn) {
-    this.status = PENDING;
-    this.value = null;
-    this.reason = null;
-    this.resArr = [];
-    this.rejArr = [];
     const resolve = (val) => {
-      this.value = val;
-      this.status = FULFILLED;
-      this.resArr.forEach((func) => func());
+      setTimeout(() => {
+        this.value = val;
+        this.resArr.forEach((func) => func());
+        this.status = statusEnum.FULLFILED;
+      });
     };
-    const reject = (reason) => {
-      this.reason = reason;
-      this.status = REJECTED;
-      this.rejArr.forEach((func) => func());
+    const reject = (val) => {
+      setTimeout(() => {
+        this.reason = val;
+        this.rejArr.forEach((func) => func());
+        this.status = statusEnum.REJECTED;
+      });
     };
-    fn(resolve, reject);
+    try {
+      fn(resolve, reject);
+    } catch (err) {
+      reject(err);
+    }
   }
-  then(onFulfilled, onReject) {
-    const promise2 = new myPromise((res, rej) => {
-      if (this.status === FULFILLED) {
-        setTimeout(() => {
-          const x = onFulfilled(this.value);
-          resolvePromise(promise2, x, res, rej);
-        });
-      } else if (this.status === PENDING) {
+  then(onResolve, onReject) {
+    let promise1;
+    promise1 = new MyPromise((resolve, reject) => {
+      if (this.status === statusEnum.PENDING) {
+        // resolve、reject会在setTimeout后执行，所以肯定能获取到promise1
         this.resArr.push(() => {
-          const x = onFulfilled(this.value);
-          resolvePromise(promise2, x, res, rej);
+          const value = onResolve(this.value);
+          resolvePromise(promise1, value, resolve, reject);
         });
         this.rejArr.push(() => {
-          onReject(this.reason);
+          const reason = onReject(this.reason);
+          resolvePromise(promise1, reason, resolve, reject);
         });
+      } else if (this.status === statusEnum.FULLFILED) {
+        setTimeout(() => {
+          const value = onResolve(this.value);
+          resolvePromise(promise1, value, resolve, reject);
+        });
+      } else {
+        // rejected状态同pending
       }
     });
-    return promise2;
+    return promise1;
   }
-  catch(fn) {
-    this.rejArr.push(fn);
-    this.status = REJECTED;
-  }
+  catch(onReject) {}
 }
-
-const test = () => {
-  return new myPromise((res, rej) => {
-    setTimeout(() => {
-      res("step 1, ok");
-    }, 1000);
-  })
-    .then((val) => {
-      console.log(val);
-      return new myPromise((res, rej) => {
-        setTimeout(() => {
-          res("step 2,ok");
-        }, 1500);
-      });
-    })
-    .then((val) => {
-      console.log(val);
-    });
-};
-
-const testFunc = async () => {
-  const ts = new myPromise((res) => {
-    setTimeout(() => {
-      res("100");
-      console.log("ok");
-    }, 2000);
-  });
-  const res = await ts;
-  console.log(res);
-};
-
-module.exports = myPromise;
